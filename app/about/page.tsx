a25563f569b945a284e7c791ef0937c3b09a3303
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-06 15:29:08 +0300

    create about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">Link to Home</Link>
+    </div>
+  );
+}
