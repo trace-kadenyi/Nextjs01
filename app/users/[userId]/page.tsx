29c69c9fd607ce7916c8ccc128bcbf571adbbb17
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-08 17:16:07 +0300

    single user page

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,52 @@
+import { Suspense } from "react";
+import type { Metadata } from "next";
+
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import UserPosts from "./components/UserPosts";
+
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ userId: string }>;
+}): Promise<Metadata> {
+  const { userId } = await params;
+
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
+
+export default async function UserPage({
+  params,
+}: {
+  params: Promise<{ userId: string }>;
+}) {
+  const { userId } = await params;
+
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+  //   const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  const user = await userData;
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
+}
