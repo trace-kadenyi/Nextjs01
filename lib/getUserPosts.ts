ac17b8661defa7786d6ff7204351f3a5c2068f59
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-08 17:16:38 +0300

    get user posts

---
+++ lib/getUserPosts.ts
@@ -0,0 +1,10 @@
+export default async function getUserPosts(userId: string) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    { next: { revalidate: 60 } }
+  );
+
+  if (!res.ok) throw new Error("failed to fetch user posts");
+
+  return res.json();
+}
