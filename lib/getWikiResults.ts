2128896b3a99708b5f8556b31e6f134c5beba8bf
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-09 17:37:15 +0300

    fix bug: gsrsearch case

--- lib/getWikiResults.ts
+++ lib/getWikiResults.ts
@@ -2,7 +2,7 @@ export default async function getWikiResults(searchTerm: string) {
   const searchParams = new URLSearchParams({
     action: "query",
     generator: "search",
-    gsrSearch: searchTerm,
+    gsrsearch: searchTerm,
     gsrlimit: "20",
     prop: "pageimages|extracts",
     exchars: "100",
