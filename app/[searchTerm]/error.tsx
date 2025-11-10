5fd86bcada8db9111624e270209d33239b6d6990
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-10 17:42:00 +0300

    error page

---
+++ app/[searchTerm]/error.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React, { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, []);
+  return (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 px-4 min-h-screen">
+      <h2 className="my-4 text-2xl font-bold">Something went wrong!</h2>
+      <button
+        className="mb-4 p-4 bg-red-500 text-white rounded-xl"
+        onClick={
+          // attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+      <p className="text-xl">
+        Or to back to <Link href="/">Home 🏡</Link>
+      </p>
+    </main>
+  );
+}
