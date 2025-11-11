9af569b518b7e6c89e85f0615bb6bd6bc2749abd
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-11 15:18:48 +0300

    create baseline code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={inter.className}>
-    
+    <main className="px-6 mx-auto">
+      <p className="my-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitspace-nowrap">
+          I'm <span className="font-bold">Tracey</span>.
+        </span>
+      </p>
     </main>
-  )
+  );
 }
