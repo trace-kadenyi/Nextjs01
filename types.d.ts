c093e484f263904852e487a66970b66b3e74c1ff
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-11 16:54:50 +0300

    add blogposts type

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,5 @@
-type Result = {
-  pageid: string;
+type BlogPost = {
+  id: string;
   title: string;
-  extract: string;
-  thumbnail?: {
-    source: string;
-    width: number;
-    height: number;
-  };
-};
-
-type SearchResult = {
-  query?: {
-    pages?: Result[];
-  };
+  date: string;
 };
