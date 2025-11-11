be3f965a9b6d0bd06d03fe4dcb5708e3ea2367fb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-11 16:54:57 +0300

    display blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Posts from "./components/Posts";
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
@@ -7,6 +8,7 @@ export default function Home() {
           I'm <span className="font-bold">Tracey</span>.
         </span>
       </p>
+      <Posts />
     </main>
   );
 }
