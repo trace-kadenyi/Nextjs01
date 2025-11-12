6896057654de1e61bbda9e964f9656f92b92afa0
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-12 20:47:54 +0300

    remove

--- src/app/thank-you/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ThankYou() {
-  return (
-    <main className="text-3xl grid place-content-center min-h-screen">
-      <h1>Thank you for your feedback!</h1>
-    </main>
-  );
-}
