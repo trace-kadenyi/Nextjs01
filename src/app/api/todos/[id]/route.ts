be81a3d340ce829402edcb6b7896c64996886641
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-12 21:52:07 +0300

    use params to obtain id

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,9 +2,16 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 // GET
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+export async function GET(request: Request, { params: { id } }: Props) {
+  //   const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
   const todo: Todo = await res.json();
