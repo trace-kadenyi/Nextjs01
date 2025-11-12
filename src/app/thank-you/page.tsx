23a84613150e4ceedf5523a2ca3b20b81e704e0e
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-12 17:14:49 +0300

    thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,7 @@
+export default function ThankYou() {
+  return (
+    <main className="text-3xl grid place-content-center min-h-screen">
+      <h1>Thank you for your feedback!</h1>
+    </main>
+  );
+}
