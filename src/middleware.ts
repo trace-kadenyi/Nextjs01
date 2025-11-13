521de5b4d97ee8f6f7cba26c7786ce24d55b0bd3
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-13 16:44:31 +0300

    add different methods

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request: Request) {
+  // METHOD 1
+  // const regex = new RegExp('/api/*')
+  // if(regex.test(request.url)) {
+
+  // }
+
+  // METHOD 2
+  // if(request.url.includes('/api/*')) {
+
+  // }
+
   console.log(request.method);
   console.log(request.url);
 
@@ -9,3 +20,8 @@ export function middleware(request: Request) {
 
   return NextResponse.next();
 }
+
+// METHOD 3
+export const config = {
+  matcher: "/api/:path*",
+};
