e1269c45d9b7ddc8ea4cd4b62a462193fe753c40
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-17 14:20:56 +0300

    block bad requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,16 +13,22 @@ export function middleware(request: Request) {
   // METHOD 2
   // if(request.url.includes('/api/*')) {}
 
-   const origin = request.headers.get("origin");
+  const origin = request.headers.get("origin");
   console.log(origin);
 
-  
+  if (origin && !allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad Request",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
 
-  console.log('Middleware')
+  console.log("Middleware");
   console.log(request.method);
   console.log(request.url);
-
- 
 
   return NextResponse.next();
 }
