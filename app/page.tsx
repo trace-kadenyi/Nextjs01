49ebe9c89ffad0554ce3801b45502d772dd68ecb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-17 14:44:40 +0300

    create app folder

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Posts from "./components/Posts";
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="my-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitspace-nowrap">
+          I&apos;m <span className="font-bold">Tracey</span>.
+        </span>
+      </p>
+      <Posts />
+    </main>
+  );
+}
