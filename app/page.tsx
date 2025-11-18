8e8a173510ad35f589fb4c69164fe1941a32218b
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-18 13:46:15 +0300

    add padding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export const revalidate = 10;
 
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
+    <main className="px-6 mx-auto mb-20">
       <p className="my-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitspace-nowrap">
