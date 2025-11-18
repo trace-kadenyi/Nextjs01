1170e483c64bc72eea7a64cde91c0db4cb0eb99a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-18 13:46:08 +0300

    add padding

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -39,7 +39,7 @@ export default async function Post({ params }: { params: { postId: string } }) {
   const pubDate = getFormattedDate(date);
 
   return (
-    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto mb-10">
       <h1 className="text-3xl mt-4 mb-0">{title}</h1>
       <p className="mt-0">{pubDate}</p>
       <article>
