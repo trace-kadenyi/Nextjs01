e650b75d3aea997694ad8808de3fbf8082f5ba0a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-19 12:09:49 +0300

    home page modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import TodoList from "./components/TodoList";
-import AddTodo from "./components/AddTodo";
+import Posts from "./components/Posts";
 
-export const revalidate = 0;
+export const revalidate = 10;
 
 export default function Home() {
   return (
-    <>
-      <AddTodo />
-      {/* @ts-expect-error Server Component */}
-      <TodoList />
-    </>
+    <main className="px-6 mx-auto mb-20">
+      <p className="my-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitspace-nowrap">
+          I&apos;m <span className="font-bold">Tracey</span>.
+        </span>
+      </p>
+      <Posts />
+    </main>
   );
 }
