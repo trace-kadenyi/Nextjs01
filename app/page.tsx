426fce29afcffe326fb10464ee18d7fd47120c8c
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-22 13:58:15 +0300

    modify welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="px-6 mx-auto mb-20">
       <p className="my-12 text-3xl text-center dark:text-white">
-        Hello and Welcome 👋&nbsp;
+        Hello and Welcome 👋.
         <span className="whitspace-nowrap">
           I&apos;m <span className="font-bold">Tracey</span>.
         </span>
