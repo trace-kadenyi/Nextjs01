09a0bf3a48938c459da61d8c0540d617dd7dbc17
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-24 14:08:55 +0300

    organize file

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -9,6 +9,7 @@ type Props = {
 export default function ListItem({ post }: Props) {
   const { id, title, date } = post;
   const formattedDate = getFormattedDate(date);
+
   return (
     <li className="mt-4 text-2xl dark:text-white/90" key={id}>
       <Link
