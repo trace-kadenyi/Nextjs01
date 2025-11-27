245a6417d8dc2e2e6b46c13f43f1273648ad8d11
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-27 16:00:20 +0300

    getPostsMeta func

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,56 +1,71 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html";
+// import fs from "fs";
+// import path from "path";
+// import matter from "gray-matter";
+// import { remark } from "remark";
+// import html from "remark-html";
 
-const postsDirectory = path.join(process.cwd(), "blogposts");
+// const postsDirectory = path.join(process.cwd(), "blogposts");
 
-// get sorted post data
-export function getSortedPostsData() {
-  // get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // remove .md from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+// // get sorted post data
+// export function getSortedPostsData() {
+//   // get file names under /posts
+//   const fileNames = fs.readdirSync(postsDirectory);
+//   const allPostsData = fileNames.map((fileName) => {
+//     // remove .md from file name to get id
+//     const id = fileName.replace(/\.md$/, "");
 
-    // read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+//     // read markdown file as string
+//     const fullPath = path.join(postsDirectory, fileName);
+//     const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    // use gray matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+//     // use gray matter to parse the post metadata section
+//     const matterResult = matter(fileContents);
 
-    const blogPost: BlogPost = {
-      id,
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-    };
+//     const blogPost: BlogPost = {
+//       id,
+//       title: matterResult.data.title,
+//       date: matterResult.data.date,
+//     };
 
-    return blogPost;
-  });
-  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
-}
+//     return blogPost;
+//   });
+//   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+// }
 
-// get post data
-export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+// // get post data
+// export async function getPostData(id: string) {
+//   const fullPath = path.join(postsDirectory, `${id}.md`);
+//   const fileContents = fs.readFileSync(fullPath, "utf8");
 
-  const matterResult = matter(fileContents);
+//   const matterResult = matter(fileContents);
 
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
+//   const processedContent = await remark()
+//     .use(html)
+//     .process(matterResult.content);
 
-  const contentHtml = processedContent.toString();
+//   const contentHtml = processedContent.toString();
 
-  const blogPostWithHtml: BlogPost & { contentHtml: string } = {
-    id,
-    title: matterResult.data.title,
-    date: matterResult.data.date,
-    contentHtml,
-  };
+//   const blogPostWithHtml: BlogPost & { contentHtml: string } = {
+//     id,
+//     title: matterResult.data.title,
+//     date: matterResult.data.date,
+//     contentHtml,
+//   };
 
-  return blogPostWithHtml;
+//   return blogPostWithHtml;
+// }
+
+export async function getPostsMeta(): Promise<Meta[] | undefined> {
+  const res = await fetch(
+    "https://api.github.com/repos/gitdagray/test-blogposts/git/trees/main?recursive=1",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GTIHUB_TOKEN}`,
+        "x-Github-Api-Version": "2022-11-28",
+      },
+    }
+  );
+
+  if(!res.ok) return undefined
 }
