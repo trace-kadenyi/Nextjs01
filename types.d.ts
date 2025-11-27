63f30c423bb90e17625eb8c6b1ca435e1044d06b
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-27 16:11:08 +0300

    blogpost type

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,11 @@
-type BlogPost = {
-  id: string;
-  title: string;
-  date: string;
-};
-
 type Meta = {
   id: string;
   title: string;
   date: string;
   tags: string[];
+};
+
+type BlogPost = {
+  meta: Meta;
+  content: any;
 };
