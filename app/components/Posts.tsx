4035de163dce9d9794b1033de96ce5ed5b4dbd3e
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-27 15:50:43 +0300

    Name posts page

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -7,7 +7,7 @@ export default function Posts() {
 
   return (
     <section className="mt-6 mx-auto max-w-2xl">
-      <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2>
+      <h2 className="text-4xl font-bold dark:text-white/90">Blogs</h2>
       <ul className="w-full">
         {posts.map((post) => (
           <ListItem key={post.id} post={post} />
